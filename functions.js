3e6c3118fd299ad2e6a801ff16077ba697cd8119
Josh Wentworth <contact@joshwentworth.com>
2021-05-20 07:57:08 -0700

    kickoff complete

---
+++ functions.js
@@ -0,0 +1,27 @@
+// select the element - querySelector
+
+let replaceInnerTEXT = function( string ) {
+// replace element's inner text
+
+}
+
+
+replaceInnerTEXT("hello")
+
+// order of code
+// 1. select element - line 1
+// 2. assign variable - live 3
+// 3. call the function - line 9
+// 4. a lot more code - line 4
+// 5. from line 9 variable string = "hello"
+
+// select element2 = <UL> - queryselector
+
+let appendListItem = function( string ) {
+// create <li>
+// add string to the li
+// append li to ul
+
+}
+
+appendListItem("hello") // all functions have implied return undefined
